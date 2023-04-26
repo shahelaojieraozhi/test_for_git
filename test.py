f96ce3ec6320024f5038995e556745e8e8622aa7
shahelaojieraozhi <1445692095@qq.com>
2023-04-26 16:51:02 +0800

    init commit

---
+++ test.py
@@ -0,0 +1,18 @@
+"""
+python3
+-- coding: utf-8 --
+-------------------------------
+@Author : RAO ZHI
+@Email : raozhi@mails.cust.edu.cn
+-------------------------------
+@File : test.py
+@Software : PyCharm
+@Time : 2023/4/26 16:48
+-------------------------------
+"""
+
+
+print("hello rao zhi")
+
+
+print('1')
